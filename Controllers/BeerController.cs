746b3b2fbc56824923976379caa3425d13b6a1fe
Enner <enner.escobedo@gmail.com>
2025-02-18 15:39:33 -0600

    Add view to get beers@

---
+++ Controllers/BeerController.cs
@@ -0,0 +1,22 @@
+﻿using introASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace introASP.Controllers
+{
+    public class BeerController : Controller
+    {
+        private readonly CursoMVCCoreContext _context;
+
+        public BeerController(CursoMVCCoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var beers = _context.Beers.Include(b => b.Brand);
+            return View(await beers.ToListAsync());
+        }
+    }
+}
