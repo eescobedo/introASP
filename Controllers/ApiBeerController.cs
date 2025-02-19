fe1cb2ab01df7fd985d935eb3e35256444eb92d8
Enner <enner.escobedo@gmail.com>
2025-02-19 16:14:24 -0600

    Add endpoint to get all beers with brands

---
+++ Controllers/ApiBeerController.cs
@@ -0,0 +1,28 @@
+﻿using introASP.Models;
+using introASP.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace introASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiBeerController : ControllerBase
+    {
+        private readonly CursoMVCCoreContext _context;
+
+        public ApiBeerController(CursoMVCCoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<BeerBrandViewModel>> Get()
+            => await _context.Beers.Include(b=> b.Brand)
+            .Select(b=> new BeerBrandViewModel
+            {
+                Name = b.Name,
+                Brand = b.Brand.Name
+            }).ToListAsync();            
+    }
+}
